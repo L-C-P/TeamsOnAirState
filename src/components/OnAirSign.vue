fdb470098f779537e79f07d4dfb5a92d768e28d0
Denis Sowa <denis@sowa.info>
2023-03-31 15:53:07 +0200

    Add not connected message

--- src/components/OnAirSign.vue
+++ src/components/OnAirSign.vue
@@ -1,16 +1,22 @@
 <template>
-    <div v-if="connected" :class="{ 'in-meeting': isInMeeting}" class="border style-neon1">
+    <div v-if="connected" :class="{ 'in-meeting': isInMeeting }" class="border style-neon1">
         <div class="on-air">On Air</div>
         <div class="toggles">
             <div>
+                Mic off
                 <div v-if="!isMuted && isInMeeting" class="toggle-on">Mic on</div>
                 <div v-if="isMuted || !isInMeeting" class="toggle-off">Mic off</div>
             </div>
             <div>
+                Cam off
                 <div v-if="isCameraOn && isInMeeting" class="toggle-on">Cam on</div>
                 <div v-if="!isCameraOn || !isInMeeting" class="toggle-off">Cam off</div>
             </div>
         </div>
+    </div>
+
+    <div v-if="!connected" class="no-connection style-neon1">
+        No Connection
     </div>
 </template>
 
